68c66e16d95546f356955526633265bcc428b47b
Kirya <kkodolov40@gmail.com>
2025-08-13 10:29:09 +0300

    Фикс core

--- src/main/resources/static/js/components/mobile-menu.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.navbar-toggler').forEach(select => {
-    select.addEventListener('click', function() {
-        document.querySelector('.navbar-collapse').classList.toggle('show');
-    });
-});
